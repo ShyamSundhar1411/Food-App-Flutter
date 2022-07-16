273f056c7f0f69791bd5696ff7155d36afe79df3
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2022-07-16 12:01:37 +0530

    Category Screen

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,17 +1,23 @@
 import "package:flutter/material.dart";
+import '../data/dummy_data.dart';
+import '../widgets/categoryItemWidget.dart';
 
 class CategoryScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(children: <Widget>[
-      
-    ],
+    return Scaffold(appBar: AppBar(title: const Text('DeliMeals')),body:GridView(
+      children: DUMMY_CATEGORIES
+          .map(
+            (categoryData) =>
+                CategoryItemWidget(categoryData.title, categoryData.color),
+          )
+          .toList(),
       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
         maxCrossAxisExtent: 200,
-        childAspectRatio: 3/2,
+        childAspectRatio: 3 / 2,
         crossAxisSpacing: 20,
         mainAxisSpacing: 20,
-        ),
-    );
+      ),
+    ));
   }
 }

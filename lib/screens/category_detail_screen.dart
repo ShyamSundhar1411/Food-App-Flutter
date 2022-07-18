47071053de0cefb476b8d002231bee99f69bcf54
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2022-07-18 12:10:24 +0530

    Meal List

--- lib/screens/category_detail_screen.dart
+++ lib/screens/category_detail_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../widgets/mealDisplayWidget.dart';
+import '../data/dummy_data.dart';
 
 class CategoryDetailScreen extends StatelessWidget {
   static const routeName = '/categorydetails';
@@ -8,9 +10,17 @@ class CategoryDetailScreen extends StatelessWidget {
         ModalRoute.of(context).settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'];
     final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
 
     return Scaffold(
         appBar: AppBar(title: Text("${categoryTitle}")),
-        body: Center(child: Text("The Recipes for the category")));
+        body: ListView.builder(
+          itemBuilder: ((context, index) {
+            return MealDisplayWidget(categoryMeals[index]);
+          }),
+          itemCount: categoryMeals.length,
+        ));
   }
 }
---
+++ lib/widgets/mealDisplayWidget.dart
@@ -0,0 +1,116 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import '../models/meal.dart';
+
+class MealDisplayWidget extends StatelessWidget {
+  final Meal meal;
+  MealDisplayWidget(this.meal);
+  void onTapShowDetail() {
+    print("Hey works");
+  }
+
+  String get ComplexityText {
+    switch (meal.complexity) {
+      case Complexity.Simple:
+        return 'Simple';
+        break;
+      case Complexity.Challenging:
+        return 'Challenging';
+        break;
+      case Complexity.Hard:
+        return 'Hard';
+        break;
+      default:
+        return 'Unknown';
+        break;
+    }
+  }
+  String get AffordabilityText {
+    switch (meal.affordability) {
+      case Affordability.Affordable:
+        return 'Affordable';
+        break;
+      case Affordability.Pricey:
+        return 'Pricey';
+        break;
+      case Affordability.Luxurious:
+        return 'Luxurious';
+        break;
+      default:
+        return 'Unknown';
+        break;
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTapShowDetail,
+      child: Card(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+          elevation: 4,
+          margin: EdgeInsets.all(10),
+          child: Column(
+            children: <Widget>[
+              Stack(
+                children: <Widget>[
+                  ClipRRect(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(15),
+                      topRight: Radius.circular(15),
+                    ),
+                    child: Image.network(
+                      meal.imageUrl,
+                      height: 250,
+                      width: double.infinity,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 20,
+                    right: 10,
+                    child: Container(
+                      width: 300,
+                      padding:
+                          EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                      color: Colors.black54,
+                      child: Center(
+                        child: Text(
+                          meal.title,
+                          style: TextStyle(fontSize: 26, color: Colors.white),
+                          softWrap: true,
+                          overflow: TextOverflow.fade,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Row(children: <Widget>[
+                        Icon(Icons.schedule),
+                        SizedBox(width: 6),
+                        Text('${meal.duration} min'),
+                      ]),
+                      Row(children: <Widget>[
+                        Icon(Icons.work),
+                        SizedBox(width: 6),
+                        Text('${ComplexityText}'),
+                      ]),
+                      Row(children: <Widget>[
+                        Icon(Icons.attach_money),
+                        SizedBox(width: 6),
+                        Text('${AffordabilityText}'),
+                      ])
+                    ],
+                  ))
+            ],
+          )),
+    );
+  }
+}
